26d78f031c75ef0584c7d04dd6529ba8d0ee0c11
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-04-19 17:35:05 +0200

    Add Song class

---
+++ src/com/lists/playlist/Song.java
@@ -0,0 +1,15 @@
+package com.lists.playlist;
+
+class Song {
+    String title;
+    double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
