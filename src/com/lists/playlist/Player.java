92a252bd4616f46aed22c09ed0b623f63187459b
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-04-21 18:11:48 +0200

    Add findSongInPlaylist(String) and deleteSongFromPlaylist(). Start implementing findSongInAlbums(String) and Album class

--- src/com/lists/playlist/Player.java
+++ src/com/lists/playlist/Player.java
@@ -1,5 +1,6 @@
 package com.lists.playlist;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -85,6 +86,54 @@ public class Player {
         playList.add(new Song(title, duration));
     }
 
+    private void deleteSongFromPlaylist() {
+        System.out.print("Enter the title you need to be removed: ");
+        String title = scanner.nextLine();
+        ArrayList<Integer> foundPositions = findSongInPlaylist(title);
+
+        if(foundPositions == null) {
+            System.out.println("The playlist is empty at the moment so " + title + " could not be deleted. Try adding some songs to the playlist first");
+            return;
+        }
+
+        if(foundPositions.size() == 0) {
+            System.out.println(title + " not found in your playlist");
+            return;
+        }
+
+        for(int i = 0; i < foundPositions.size(); i++) {
+            playList.remove(foundPositions.get(i));
+        }
+    }
+
+    private ArrayList<Integer> findSongInPlaylist(String title) {
+        if(playList.isEmpty()) {
+            return null;
+        }
+
+        ArrayList<Integer> positions = new ArrayList<>();
+
+        for(int i = 0; i < playList.size(); i++) {
+            if(playList.get(i).getTitle().equals(title)) {
+                positions.add(i);
+            }
+        }
+
+        return positions;
+    }
+
+    private ArrayList<PairNumberSong> findSongInAlbums(String title) {
+        if(albums.isEmpty()) {
+            return null;
+        }
+
+        ArrayList<PairNumberSong> foundPairs = new ArrayList<>();
+
+        for(int i = 0; i < albums.size(); i++) {
+            for(int j = 0; j < albums.get(i).)
+        }
+    }
+
     private void printActions() {
         System.out.println("Press:");
         System.out.println("1 - to play next song");
@@ -102,5 +151,28 @@ public class Player {
         System.out.println("12 - to show content of a specific album");
         System.out.println("13 - to print available actions");
         System.out.println("0 - to quit the program");
+    }
+
+
+
+
+    // Inner class for using it further in ArrayList of positions of searched songs (album number and the number of song in a given album)
+    // One song can only appear in one album once, however it can re-appear in any other album
+    private class PairNumberSong {
+        private int albumNumber;
+        private int songNumberInAlbum;
+
+        public PairNumberSong(int albumNumber, int songNumberInAlbum) {
+            this.albumNumber = albumNumber;
+            this.songNumberInAlbum = songNumberInAlbum;
+        }
+
+        public int getAlbumNumber() {
+            return albumNumber;
+        }
+
+        public int getSongNumberInAlbum() {
+            return songNumberInAlbum;
+        }
     }
 }
